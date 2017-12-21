17979b94f313cd2d8e4accc545bf47434a8bfaf1
mohamed mahmoud jedeine <moud@mohameds-MacBook-Air.local>
2017-12-21 12:47:34 -0500

    styling the deleted articles

--- routes/routes.js
+++ routes/routes.js
@@ -58,6 +58,7 @@ router.post('/save/:id', function(req, res){
           }         
         }
       });
+ res.render('index',{articles:result});
  });
 router.get('/articlesaved', function(req, res){
   Articles.find().sort({ createdAt: -1 }).exec(function(err, data) { 
